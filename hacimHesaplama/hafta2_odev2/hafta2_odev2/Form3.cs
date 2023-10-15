10cf190c454d0b267bcee5abd14c9509352ad376
ŞEVVAL KOÇ <112202225+sevvalkoc64@users.noreply.github.com>
2023-10-15 15:47:53 +0300

    Add files via upload

---
+++ hacimHesaplama/hafta2_odev2/hafta2_odev2/Form3.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta2_odev2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int c = Convert.ToInt32(textBox3.Text);
+
+            int yuzeyAlani = 2 * ((a * b) + (b * c) + (a * c));
+            int kesitAlani = a * b;
+            int hacim = a * b * c;
+
+            label4.Text = "Yüzey alanı: "+ Convert.ToString(yuzeyAlani);
+            label5.Text = "Hacim: " + Convert.ToString(hacim);
+            label6.Text = "Kesit alanı: " + Convert.ToString(kesitAlani);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar)== 13)
+            {
+                textBox2.Focus();
+            }
+        }
+
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Yellow;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                textBox3.Focus();
+            }
+        }
+
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.Yellow;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.White;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(Convert.ToInt32(e.KeyChar)== 13)
+            {
+                textBox1.Focus();
+            }
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            textBox3.BackColor = Color.Yellow;
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            textBox3.BackColor = Color.White;
+        }
+    }
+}
---
+++ hacimHesaplama/hafta2_odev2/hafta2_odev2/Form6.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hafta2_odev2
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int r = Convert.ToInt32(textBox1.Text);
+            int pi = Convert.ToInt32(Math.PI);
+
+            int yuzeyAlani = 4 * pi * (r * r);
+            int kesitAlani = pi * (r * r);
+            int hacim = (4 * pi * (r * r * r))/3;
+
+            label2.Text = "Yüzey alanı: "+Convert.ToString(yuzeyAlani);
+            label3.Text = "Kesit alanı: " + Convert.ToString(kesitAlani);
+            label4.Text = "Hacim: " + Convert.ToString(hacim);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Yellow;
+        }
+    }
+}
